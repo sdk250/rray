3c2714e8adc1fb70af2b3872a04489ce2e913df6
sdk250 <hexfya@gmail.com>
2026-03-27 12:25:01 -0400

    chore: sync code

--- src/driver.rs
+++ src/driver.rs
@@ -1,5 +1,23 @@
+use tokio::{
+    net::TcpStream,
+    time::{ timeout, Duration }
+};
+
 use crate::error::{ Result, RError };
 
+const SERVER_HOST: &'static str = "192.168.3.2";
+const SERVER_PORT: u16 = 20881;
+
 pub(crate) async fn handle_connection() -> Result<()> {
+    let mut server_socket = timeout(
+        Duration::from_secs(5),
+        TcpStream::connect((SERVER_HOST, SERVER_PORT))
+    ).await??;
+
+    // Set TCP_NODELAY to disable Nagle's algorithm for low-latency communication
+    server_socket.set_nodelay(true)?;
+
+    let mut csprng = rand::thread_rng();
+
     Ok(())
 }
--- src/error.rs
+++ src/error.rs
@@ -2,6 +2,7 @@ use std::{
     io::Error as IoError,
     sync::Arc
 };
+use tokio::time::error::Elapsed as TimeoutError;
 use thiserror::Error;
 
 pub type Result<T> = std::result::Result<T, RError>;
@@ -10,6 +11,9 @@ pub type Result<T> = std::result::Result<T, RError>;
 pub enum RError {
     #[error("I/O error: {0}")]
     IO(#[from] IoError),
+
+    #[error("Timeout error: {0}")]
+    Timeout(#[from] TimeoutError),
 
     #[error("An error occurred in {0}: {1}")]
     Generic(&'static str, Arc<str>),
